bcc1bbade39990b647fa7b4929d82d547c665a38
dariocapdevila <dariocapdevila@gmail.com>
2025-03-08 22:42:56 -0300

    Add Attendance seeder and migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             PositionSeeder::class,
-            EmployeeSeeder::class
+            EmployeeSeeder::class,
+            AttendanceSeeder::class,
         ]);
 
 
